bf849ee005657f022177af3e600d53e518b76402
mahlet1626 <mahlettesfaye0@gmail.com>
2023-02-01 21:52:32 +0300

    all tickets

--- src/containers/Tickets/AllTickets/index.js
+++ src/containers/Tickets/AllTickets/index.js
@@ -10,8 +10,7 @@ import LayoutContentWrapper from '../../../components/utility/layoutWrapper.js';
 import Box from '../../../components/utility/box';
 import ContentHolder from '../../../components/utility/contentHolder';
 import Popconfirms from '../../../components/feedback/popconfirm';
-import { Col } from 'antd';
-import { Link } from 'react-router-dom';
+import { Row, Col } from 'antd';
 import {
   ActionBtn,
   Fieldset,
@@ -22,51 +21,20 @@ import {
   ActionWrapper,
   ComponentTitle,
   TableWrapper,
-  // StatusTag,
+  StatusTag,
 } from './articles.style';
 import clone from 'clone';
-import { Row } from 'antd';
-import { notification } from "antd";
-import { Icon } from 'antd';
-import { FilterDropdown } from '../../../components/tables/helperCells';
 import axios from 'axios';
-// const openNotificationWithIcon = (type) => {
-//   notification[type]({
-//     message: "",
-//     description:
-//       "...Loading",
-//   });
-// };
-const openErrorNotificationWithIcon = (type) => {
-  notification[type]({
-    message: "Error",
-    description: "An error occurred, please try again",
-  });
-};
 
-class Ticket extends Component {
-  constructor(props) {
-    super(props);
-    this.onSearch = this.onSearch.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
-    this.state = {
-      dataList: [],
-      filterDropdownVisible: false,
-      searchText: '',
-      filtered: false
-    };
-  }
-  state = {
-    dataList: this.props.tickets,
-    filterDropdownVisible: false,
-    searchText: '',
-    filtered: false,
+
+
+class Tickets extends Component {
+  state={
     users:[],
     projects:[],
-
   }
   async componentDidMount() {
-    await this.props.getTickets();
+    await this.props.loadFromFireStore();
 
     await axios.get("http://localhost:3000/api/users/").then((response) => {
         this.setState({users: response.data})     
@@ -77,55 +45,13 @@ class Ticket extends Component {
     });
 
   }
-  onSearch() {
-    let { searchText } = this.state;
-    searchText = searchText.toUpperCase();
-    const dataList = this.props.tickets
-      .filter(data => data['name'].toUpperCase().includes(searchText));
-    console.log(dataList)
-    this.setState({
-      dataList: dataList,
-      filterDropdownVisible: false,
-      searchText: '',
-      filtered: false
-    });
-
-    // else {
-    //   this.setState({
-    //     dataList,
-    //     filterDropdownVisible: false,
-    //     searchText: '',
-    //     filtered: false
-    //   });
-    // }
-
-  }
-
-  
-
-
-  onInputChange(event) {
-    this.setState({ searchText: event.target.value });
-  }
-  componentWillReceiveProps(nextProps) {
-    console.log(nextProps)
-    if (nextProps.errorMessage == "There is a loading problem") {
-      openErrorNotificationWithIcon("error")
-    }
-    // else if (nextProps.isLoading) {
-    //   openNotificationWithIcon("info")
-    // }
-  }
-
- 
-
   handleRecord = async (actionName, ticket) => {
     if (ticket.key && actionName !== 'delete') actionName = 'update';
     await this.props.saveIntoFireStore(ticket, actionName);
   };
-  // resetRecords = () => {
-  //   // this.props.resetFireStoreDocuments();
-  // };
+  resetRecords = () => {
+    this.props.resetFireStoreDocuments();
+  };
 
   handleModal = (ticket = null) => {
     console.log(ticket)
@@ -133,24 +59,17 @@ class Ticket extends Component {
   };
 
   onRecordChange = (key, event) => {
-    let { ticket} = clone(this.props);
-
+    let { ticket } = clone(this.props);
     if (key) ticket[key] = event.target.value;
     this.props.update(ticket);
   };
-
-  
 
   onSelectChange = (key, value) => {
     let { ticket } = clone(this.props);
     if (key) ticket[key] = value;
     this.props.update(ticket);
   };
-  handleImageChange = (e) => {
-    this.setState({
-      image: e.target.files[0],
-    });
-  };
+  assigned_to
   userValue = ()=>{
     const { ticket } = clone(this.props);
     if (ticket.assigned_to != null && ticket.assigned_to['name']){
@@ -172,7 +91,7 @@ class Ticket extends Component {
   }
 
   render() {
-    const { modalActiveTicket, tickets } = this.props;
+    const { modalActive, tickets } = this.props;
     const { ticket } = clone(this.props);
     const dataSource = [];
     Object.keys(tickets).map((ticket, index) => {
@@ -180,10 +99,11 @@ class Ticket extends Component {
         ...tickets[ticket],
         key: ticket,
       });
-      
     });
 
-
+    const rowSelection = {
+      onChange: (selectedRowKeys, selectedRows) => { },
+    };
 
     const columns = [
      
@@ -192,7 +112,6 @@ class Ticket extends Component {
         dataIndex: 'issue_title',
         key: 'issue_title',
         width: '200px',
-       
         
         // sorter: (a, b) => {
         //   if (a.name < b.name) return -1;
@@ -225,7 +144,17 @@ class Ticket extends Component {
           if (a.issue_priority > b.issue_priority) return 1;
           return 0;
         },
-
+        // render: (text, row) => {
+        //   return (
+        //     row.issue_priority == "Low" ? (
+              
+        //         <b color="#f50">{row.priority}</b>
+        //     ) :row.issue_priority == "Medium" ? (
+        //       <b color="#90EE90">{row.priority}</b>
+        //       ): (   <b color="#808080">{row.priority}</b>
+        //     )
+        //   );
+        // },
       },
       {
         title: 'Assigned to',
@@ -250,7 +179,16 @@ class Ticket extends Component {
           if (a.issue_status > b.issue_status) return 1;
           return 0;
         },
-
+        // render: (text, row) => {
+        //   return (
+        //     row.priority == "high" ? (
+        //       <Tag className="mr-5" color="#f50">{row.priority}</Tag>
+        //     ) :row.priority == "medium" ? (
+        //       <Tag className="mr-5" color="#90EE90">{row.priority}</Tag>
+        //       ): (<Tag className="mr-5" color="#808080">{row.priority}</Tag>
+        //     )
+        //   );
+        // },
       },
 
     
@@ -284,35 +222,39 @@ class Ticket extends Component {
       },
     ];
 
+
     return (
       <LayoutContentWrapper>
         <Box>
           <ContentHolder style={{ marginTop: 0, overflow: 'hidden' }}>
             <TitleWrapper>
               <ComponentTitle>Tickets</ComponentTitle>
+
               <ButtonHolders>
+                {/* <ActionBtn type="danger" onClick={this.resetRecords}>
+                  Reset record
+                </ActionBtn> */}
+
                 <ActionBtn
                   type="primary"
                   onClick={this.handleModal.bind(this, null)}
                 >
-                  Add Ticket
+                  Add new ticket
                 </ActionBtn>
               </ButtonHolders>
             </TitleWrapper>
+
             <Modal
-              // visible={modalActive}
-              onClose={this.props.toggleModal.bind(this, null)}
+              visible={modalActive}
+              onClose={this.handleModal.bind(this, null)}
               title={ticket.key ? 'Update Ticket' : 'Add New Ticket'}
               okText={ticket.key ? 'Update Ticket' : 'Add Ticket'}
               onOk={this.handleRecord.bind(this, 'insert', ticket)}
-              onCancel={this.props.toggleModal.bind(this, null)}
+              onCancel={this.handleModal.bind(this, null)}
             >
-              <Form id="ticket">
-              
-
-
+              <Form>
                 <Fieldset>
-                  <Label>Issue Title</Label>
+                  <Label>Title</Label>
                   <Input
                     label="issue_title"
                     placeholder="Enter Title"
@@ -320,7 +262,6 @@ class Ticket extends Component {
                     onChange={this.onRecordChange.bind(this, 'issue_title')}
                   />
                 </Fieldset>
-
                 <Fieldset>
                   <Label>Project</Label>
                       <Select
@@ -336,35 +277,35 @@ class Ticket extends Component {
                     > 
                     {
                       this.state.projects.map((project,i)=>{
-                        
                         return <Option key={project.id} value={[project.title, project.id]} >{project.title}</Option>
                       })
-                     
                     } 
   
                     </Select>
                 </Fieldset>
-
+              
+                 
+                      <Fieldset>
+                        <Label>Issue Priority</Label>
+                            <Select
+                            showSearch='true'
+                            searchValue=""
+                            placeholder="Issue Priority"
+                            value={ticket.issue_priority}
+                            onChange={this.onSelectChange.bind(this, 'issue_priority')}
+                            // style={{ width: '50%' }}
+                          >  
+                            <Option value='Low'  >Low</Option>
+                            <Option value='Medium'>Medium</Option>
+                            <Option value='High'>High</Option>
+                          </Select>
+                      </Fieldset>
+             
                 <Fieldset>
-                  <Label>Issue Priority</Label>
-                  <Select
-                    defaultValue={ticket.issue_priority}
-                    placeholder="Choose a priority"
-                    onChange={this.onSelectChange.bind(this, 'issue_priority')}
-                  >
-                      <Option value="Low">Low</Option>
-                      <Option value="Medium">Medium</Option>
-                      <Option value="High">High</Option>
-                     
-                     
-                  </Select>
-                </Fieldset>
-
-                <Fieldset>
-                  <Label>Assign to</Label>
+                  <Label>Assign To</Label>
                       <Select
                       showSearch={true}
-                      placeholder="Assigned to"
+                      placeholder="Assigned To"
                       onChange={this.onSelectChange.bind(this, 'assigned_to')}
                       value={this.userValue()}
                       filterOption={(inputValue, option) =>
@@ -382,23 +323,24 @@ class Ticket extends Component {
                     </Select>
                 </Fieldset>
 
+        
+                  
+                      <Fieldset>
+                      <Label>Issue Status</Label>
+                      <Select
+                        defaultValue={ticket.issue_status}
+                        placeholder="Status"
+                        onChange={this.onSelectChange.bind(this, 'issue_status')}
+                        // style={{ width: '50%' }}
+                      >
+                        <Option value="Started">Started</Option>
+                        <Option value="In Progress">In Progress</Option>
+                        <Option value="Fixed">Fixed</Option>
+                        <Option value="Failed">Failed</Option>
+                      </Select>
+                    </Fieldset>
+                 
                 
-                <Fieldset>
-                  <Label>Issue Status</Label>
-                  <Select
-                    defaultValue={ticket.issue_status}
-                    placeholder="Status"
-                    onChange={this.onSelectChange.bind(this, 'issue_status')}
-                  >
-                      <Option value="Started">Started</Option>
-                      <Option value="In progess">In progess</Option>
-                      <Option value="Fixed">Fixed</Option>
-                      <Option value="Failed">Failed</Option>
-                     
-                     
-                  </Select>
-                </Fieldset>
-
               </Form>
             </Modal>
             <TableWrapper
@@ -408,9 +350,9 @@ class Ticket extends Component {
               bordered={true}
               dataSource={dataSource}
               loading={this.props.isLoading}
-              className="isoSearchableTable"
+              className="isoSimpleTable"
               pagination={{
-                // defaultPageSize: 1,
+                defaultPageSize: 10,
                 hideOnSinglePage: true,
                 total: dataSource.length,
                 showTotal: (total, range) => {
@@ -421,16 +363,14 @@ class Ticket extends Component {
             />
           </ContentHolder>
         </Box>
-      </LayoutContentWrapper >
+      </LayoutContentWrapper>
     );
   }
 }
 
-
 export default connect(
-
   state => ({
     ...state.Tickets,
   }),
   actions
-)(Ticket);
+)(Tickets);
